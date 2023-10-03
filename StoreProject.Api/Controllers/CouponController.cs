da66471ab2792c3792df6d1ba23e66e3d46b788d
hieuporo <hieucobappp@gmail.com>
2023-10-03 20:46:11 +0700

    Create Skeleton of project

---
+++ StoreProject.Api/Controllers/CouponController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StoreProject.Domain.Entities;
+
+namespace StoreProject.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouponController : Controller
+    {
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var coupon = new Coupon { Id = id , CouponCode = "Hello" };
+            return Ok(coupon);
+        }
+    }
+}
---
+++ StoreProject.Domain/Entities/Coupon.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreProject.Domain.Entities
+{
+    public class Coupon
+    {
+        public int Id { get; set; }
+        public string CouponCode { get; set; }
+    }
+}
